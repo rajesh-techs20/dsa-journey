09121b31b44d38e9fd41fe1e1efb2eb80fbb8a5f
RAJESH G R <rgr98624@gmail.com>
2026-03-13 12:17:08 +0530

    Add palindrome check function
    Implement a function to check if a number is a palindrome.

---
+++ array/palindrome.py
@@ -0,0 +1,15 @@
+#check ppalindrome
+#leetcode
+
+ def isPalindrome(self, x):
+        temp=x
+        rev=0
+        while x>0:
+            digit=x%10
+            rev=rev*10+digit
+            x=x//10
+        if temp==rev:
+            return True
+        else:
+            return False
+        
