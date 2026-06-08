91567623a42f67f9ba7ad929f76aaa775a444c56
RAJESH G R <rgr98624@gmail.com>
2026-06-08 23:22:04 +0530

    Implement max subarray algorithm in maxsubarray.py

---
+++ maxsubarray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+  def maxSubarray(self,nums):
+    current_sum=nums[0]
+    best_sum=nums[0]
+    for num in nums[1:]:
+      current_sum+=num
+      if current_sum<num:
+        current_sum=num
+      if current_sum>best_sum:
+        best_sum=current_sum
+    return best_sum
