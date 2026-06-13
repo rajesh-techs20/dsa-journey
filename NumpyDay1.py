8c4c5ec0f38137df662336d67e1eaed75f3ef6f7
RAJESH G R <rgr98624@gmail.com>
2026-06-13 23:49:11 +0530

    Add basic NumPy operations in NumpyDay1.py

---
+++ NumpyDay1.py
@@ -0,0 +1,17 @@
+import numpy as np
+a=np.array([1,2,3])
+print(a)
+print(a+2)
+print(a*2)
+print(np.sum(a))
+print(np.mean(a))
+print(np.max(a))
+print(np.min(a))
+
+b=np.array([5,10,15,20,25])
+
+print(b)
+print(b+10)
+print(b*3)
+print(np.sum(b))
+print(np.mean(b))
